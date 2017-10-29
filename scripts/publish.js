786af2d7670db09e03329fa983117b5cb7799946
cyqresig <chenyiqin@meituan.com>
2017-10-29 11:30:44 +0800

    fix: fix incorrect code

--- scripts/publish.js
+++ scripts/publish.js
@@ -6,6 +6,13 @@ const base = configs.cwdPath;
 const output = configs.outputPath;
 const registry = configs.registry;
 
+export const shellLog = (file, args) => {
+    const promise = execa(file, args);
+    promise.stdout.pipe(process.stdout);
+    promise.stderr.pipe(process.stderr);
+    return promise;
+};
+
 const shell = async(file, args) => {
     const {
         code,
@@ -19,9 +26,9 @@ const shell = async(file, args) => {
 };
 
 const publish = async() => {
-    const { version } = require('../../package.json');
+    const { version } = require(path.join(base, 'package.json'));
     const branch = await shell('git', ['symbolic-ref', '--short', 'HEAD']);
-    await execLog('npm', ['run', 'build']);
+    await shellLog('npm', ['run', 'build']);
     const outputPath = path.join(base, output);
     process.chdir(outputPath);
     const publishCommands = ['publish', `--registry=${registry}`];
@@ -34,7 +41,7 @@ const publish = async() => {
     if (branch !== 'master' && version.indexOf('-') === -1) {
         throw new Error(`version ${version} is an incorrect beta version`);
     } else {
-        await execLog('npm', publishCommands);
+        await shellLog('npm', publishCommands);
     }
     process.chdir(base);
 };
